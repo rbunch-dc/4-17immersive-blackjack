98299173975c2cc9c02c509fefd2d7ecd19903d8
Robert Bunch <rob@digitalcrafts.com>
2017-05-17 12:12:10 -0400

    reset, win

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,11 @@ $(document).ready(function(){
 	$('.deal-button').click(function(){
 		// The deal stuff happens here...
 		// Here, theDeck is still a copy of freshDeck
-		shuffleDeck();
+		// shuffleDeck();
 		// Here, theDeck is shuffled, no longer in order of freshDeck
 		// console.log(theDeck);
 		// console.log(freshDeck);
+		reset();
 
 		// We have a shuffled deck, add the 1 and 3rd card to the playersHand and the DOM
 		// Do the same for the dealer
@@ -37,19 +38,124 @@ $(document).ready(function(){
 		placeCard('dealer',1,dealersHand[0]);
 		placeCard('dealer',2,dealersHand[1]);
 
+		calculateTotal(playersHand,'player');
+		calculateTotal(dealersHand,'dealer');
+
+
 	});
 
 	$('.hit-button').click(function(){
 		// Hit functionallity...
+		// Player wants a new card. This means:
+		// 1. shift OFF of theDeck
+		// 2. push on to playersHand
+		// 3. Run placeCard to put the new card (image) in the DOM
+		// 4. Run calculateTotal to find out the new hand total
+
+		if(calculateTotal(playersHand, 'player') < 21){
+			playersHand.push(theDeck.shift()); //This covers 1 & 2
+			var lastCardIndex = playersHand.length - 1;
+			var slotForNewCard = playersHand.length;
+			placeCard('player',slotForNewCard,playersHand[lastCardIndex]); //3
+			calculateTotal(playersHand, 'player'); //4
+		}
 	});
 
 	$('.stand-button').click(function(){
 		// On click stand...
+		// Player has given control over the dealer.
+		// Dealer MUST hit until dealer has 17 or more
+		var dealerTotal = calculateTotal(dealersHand,'dealer');
+		while(dealerTotal < 17){
+			// Hit works the same...
+			// 1. Push card from top of deck onto dealers hand
+			// 2. update DOM (placecard)
+			// 3. Update dealerTotal
+			dealersHand.push(theDeck.shift());
+			var lastCardIndex = dealersHand.length - 1;
+			var slotForNewCard = dealersHand.length;
+			placeCard('dealer',slotForNewCard,dealersHand[lastCardIndex]); //3
+			dealerTotal = calculateTotal(dealersHand, 'dealer'); //4
+		}
+		checkWin();
 	});
 
 	/////////////////////////////////////////////////
 	//////////////UTILITY FUNCTIONS//////////////////
 	/////////////////////////////////////////////////
+	function reset(){
+		// In order to reset the game, we need to:
+		// 1. Reset the deck.
+		theDeck = freshDeck.slice();
+		shuffleDeck();
+		// 2. Reset teh player and dealer hand arrays
+		playersHand = [];
+		dealersHand = [];
+		// 3. REset the cards in teh DOM
+		$('.card').html('');
+		// 4. Reset the totals for both players
+		$('.dealer-total-number').html('0')
+		$('.player-total-number').html('0')
+		$('.message').text('');
+
+	}
+
+
+	function checkWin(){
+		var playerTotal = calculateTotal(playersHand, 'player');
+		var dealerTotal = calculateTotal(dealersHand, 'dealer'); //4
+		var winner = "";
+		// If Player has more than 21. Player Busts.
+		if(playerTotal > 21){
+			winner = "You have busted. Dealer wins.";
+		}else if(dealerTotal > 21){
+			winner = "Dealer has busted. You win!"
+		}else{
+			// Neither player has busted. See who won...
+			if(playerTotal > dealerTotal){
+				winner = "You beat the dealer!";
+			}else if(playerTotal < dealerTotal){
+				winner = "The dealer has bested you. We get your money";
+			}else{
+				winner = "It's a push!"
+			}
+		}
+		$('.message').text(winner);
+	}
+
+	function calculateTotal(hand, who){
+		console.log(hand);
+		// hand will be an array (either playersHand or dealersHand)
+		// who will be what the DOM knows the player as (dealer or player)
+		var totalHandValue = 0;
+		var thisCardValue = 0;
+		var hasAce = false;
+		var totalAces = 0;
+		for(let i = 0; i < hand.length; i++){
+			thisCardValue = Number(hand[i].slice(0,-1));
+			if(thisCardValue > 10){
+				thisCardValue = 10;				
+			}else if(thisCardValue == 1){
+				// this is an Ace!
+				hasAce = true;
+				totalAces++;
+				thisCardValue = 11;
+			}
+			totalHandValue += thisCardValue;
+			// console.log(thisCardValue)
+		}
+		for(let i = 0; i< totalAces; i++){
+			if(totalHandValue > 21){
+				totalHandValue -= 10
+			}			
+		}
+
+		// We have teh total, now update the DOM.
+		var totalToUpdate = '.' + who + '-total-number';
+		$(totalToUpdate).text(totalHandValue);
+		return totalHandValue;
+	}
+
 	function placeCard(who, where, what){
 		// Find the DOM element, based on the args, that we want to change
 		// i.e., find the element that we want to put the image in
